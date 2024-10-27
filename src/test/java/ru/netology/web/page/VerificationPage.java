2fe89f6de853d5254b05d07970fbef6e147885dd
anelia <aneliamuhambetova@gmail.com>
2024-10-27 17:39:29 +0400

    Error commit

---
+++ src/test/java/ru/netology/web/page/VerificationPage.java
@@ -0,0 +1,23 @@
+package ru.netology.web.page;
+
+import com.codeborne.selenide.SelenideElement;
+import ru.netology.web.data.DataHelper;
+
+import static com.codeborne.selenide.Condition.visible;
+import static com.codeborne.selenide.Selenide.$;
+import static ru.netology.web.data.DataHelper.getVerificationCode;
+
+public class VerificationPage {
+    private final SelenideElement codeField = $("[data-test-id=code] input");
+    private final SelenideElement verifyButton = $("[data-test-id=action-verify]");
+
+    public VerificationPage() {
+        codeField.shouldBe(visible);
+    }
+
+    public DashboardPage validVerify(String getVerificationCode) {
+        codeField.setValue(getVerificationCode());
+        verifyButton.click();
+        return new DashboardPage();
+    }
+}
